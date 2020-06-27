ee78117f690dd61fca16b0441a44ee1ee84c7157
ALEXSUPERCOMP\alexa <alexander12356@inbox.ru>
2020-06-27 17:49:36 +0900

    Level loader Complete

---
+++ Assets/Scripts/CoreGameplay/LevelComplete.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelComplete : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        GameCycle.Instance.LevelComplete();
+    }
+}
---
+++ Assets/Scripts/MetaGameplay/GameCycle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using TMPro;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameCycle : MonoBehaviour
+{
+	public static GameCycle Instance;
+
+	private int _currentLevel = 0;
+	[SerializeField] private int _maxLevel = 0;
+
+	private void Awake()
+	{
+		Instance = this;
+		DontDestroyOnLoad(gameObject);
+	}
+
+	public void StartGame()
+	{
+		LoadLevel();
+	}
+
+	public void LevelComplete()
+	{
+		_currentLevel++;
+
+		if (_currentLevel > _maxLevel)
+		{
+			CompleteGame();
+		}
+		else
+		{
+			LoadStore();
+		}
+	}
+
+	private void CompleteGame()
+	{
+		SceneManager.LoadScene("GameComplete");
+	}
+
+	private void LoadStore()
+	{
+		SceneManager.LoadScene("Store");
+	}
+
+	public void CloseStore()
+	{
+		LoadLevel();
+	}
+
+	private void LoadLevel()
+	{
+		SceneManager.LoadScene($"Level{_currentLevel}");
+	}
+}
