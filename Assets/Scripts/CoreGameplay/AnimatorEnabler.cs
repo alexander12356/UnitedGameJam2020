5119bcb86d2bbd29a4aed8048a2cca49eda71e05
ALEXSUPERCOMP\alexa <alexander12356@inbox.ru>
2020-06-28 17:53:15 +0900

    add enemy optimization

---
+++ Assets/Scripts/CoreGameplay/AnimatorEnabler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using BR;
+
+using UnityEngine;
+
+public class AnimatorEnabler : MonoBehaviour
+{
+    public float EnableDistance = 0f;
+    public Animator _Animator = null;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(PlayerActor.Instance.transform.position, transform.position) <= EnableDistance)
+        {
+            _Animator.enabled = true;
+        }
+        else
+        {
+            _Animator.enabled = false;
+        }
+    }
+}
