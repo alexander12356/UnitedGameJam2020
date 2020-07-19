7cadef4e84a68a5351e75cbcf5914d78b83b8451
ALEXSUPERCOMP\alexa <alexander12356@inbox.ru>
2020-07-19 16:07:36 +0900

    Impelement main menu window presenter and view classes

---
+++ Assets/BadRivals/Scripts/MainMenuComponent/Window/MainMenu/View/IMainMenuWindowView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BadRivals.MainMenuComponent.Window.MainMenu.View
+{
+	public interface IMainMenuWindowView
+	{
+		event Action OnGameStartButtonPressed;
+		event Action OnSettingsButtonPressed;
+		event Action OnQuitButtonPressed;
+	}
+}
---
+++ Assets/BadRivals/Scripts/MainMenuComponent/Window/MainMenu/View/MainMenuWindowView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using UnityEngine;
+
+namespace BadRivals.MainMenuComponent.Window.MainMenu.View
+{
+	public class MainMenuWindowView : MonoBehaviour, IMainMenuWindowView
+	{
+		public event Action OnGameStartButtonPressed;
+		public event Action OnSettingsButtonPressed;
+		public event Action OnQuitButtonPressed;
+
+		public void GameStartButtonPress()
+		{
+			OnGameStartButtonPressed?.Invoke();
+		}
+
+		public void SettingsButtonPressed()
+		{
+			OnSettingsButtonPressed?.Invoke();
+		}
+
+		public void QuitButtonPress()
+		{
+			OnQuitButtonPressed?.Invoke();
+		}
+	}
+}
