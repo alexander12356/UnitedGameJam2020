e4e7d7ed2509d6489ee104a5fcaceda8ac61acf0
ALEXSUPERCOMP\alexa <alexander12356@inbox.ru>
2020-08-09 16:57:26 +0900

    load save list from SaveDataAccess

--- Assets/BadRivals/Scripts/ApplicationComponent/ISaveSystemAccess.cs
+++ Assets/BadRivals/Scripts/ApplicationComponent/ISaveSystemAccess.cs
@@ -1,6 +1,10 @@
-﻿namespace BadRivals.Application
+﻿using System;
+using System.Collections.Generic;
+
+namespace BadRivals.Application
 {
 	public interface ISaveSystemAccess
 	{
+		void GetSaveList(Action<List<IMainMenuSaveData>> onSuccess);
 	}
 }
--- Assets/BadRivals/Scripts/MainMenuComponent/Window/StartGame/Presenter/StartGameWindowPresenter.cs
+++ Assets/BadRivals/Scripts/MainMenuComponent/Window/StartGame/Presenter/StartGameWindowPresenter.cs
@@ -1,9 +1,29 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+
+using BadRivals.Application;
+
+using UnityEngine;
+
+using Zenject;
 
 namespace BadRivals.MainMenuComponent.Window.StartGame.Presenter
 {
 	public class StartGameWindowPresenter : MonoBehaviour, IStartGameWindowPresenter
 	{
+		[Inject] private readonly ISaveSystemAccess _saveSystemAccess = null;
 		
+		public void Start()
+		{
+			InitSaveList();
+		}
+
+		private void InitSaveList()
+		{
+			_saveSystemAccess.GetSaveList(SaveListGettedHandler);
+		}
+
+		private void SaveListGettedHandler(List<IMainMenuSaveData> saveList)
+		{
+		}
 	}
 }
